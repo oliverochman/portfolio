77a64470950389e0da0561a90023a247f772cabf
oliver_ochman <oliver.ochman@hotmail.com>
2017-11-16 11:32:19 +0100

    first commit

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,7 @@
+it 'displays project list' do
+    expect(page).to have_css '.projects'
+    within '.projects' do
+      expect(page).to have_content 'My First Website'
+      expect(page).to have_content 'FizzBuzz'
+    end
+  end
