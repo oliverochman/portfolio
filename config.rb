ea34aa3aed396e4055cdc13c0d77601da9f1a70c
oliver_ochman <oliver.ochman@hotmail.com>
2017-11-16 15:21:39 +0100

    add SM icons in contact page

--- config.rb
+++ config.rb
@@ -38,6 +38,16 @@ helpers do
     DateTime.now.strftime('%Y-%m-%d %H:%M')
   end
 
+  def display_social_icon(contact)
+     if contact.name == 'Github'
+       haml_tag :i, class: 'fa fa-github fa-5'
+     elsif contact.name == 'Facebook'
+       haml_tag :i, class: 'fa fa-facebook fa-5'
+     elsif contact.name == 'Instagram'
+       haml_tag :i, class: 'fa fa-instagram fa-5'
+    end
+  end
+
   def scrape_craft
     require 'open-uri'
     require 'mechanize'
